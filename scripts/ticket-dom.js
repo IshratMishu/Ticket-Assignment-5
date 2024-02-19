1e1fa98f0b5aade0ebe06e4ebb2efcf72d3f7b26
IshratMishu <ishratjahan0425@gmail.com>
2024-02-19 12:57:25 +0600

    more js added

--- scripts/ticket-dom.js
+++ scripts/ticket-dom.js
@@ -1,9 +1,9 @@
 
 let seat = 0 ;
 let availableSeats = 40 ;
+const ticketPrice = 550 ;
 
 const seatSelection = document.getElementsByClassName("kbd");
-
  for(const oneSeat of seatSelection){
      oneSeat.addEventListener('click', function(event) {
         seat = seat + 1;
@@ -27,12 +27,41 @@ const seatSelection = document.getElementsByClassName("kbd");
         tr.appendChild(td3) ;
         firstSeat.appendChild(tr);    
 
+
+const button = document.getElementById("apply-btn");
+console.log(button);
+
+// const totalPrice = document.getElementById("total-price").innerText;
+// const convertTotalPrice = parseInt(totalPrice);
+// const sum = convertTotalPrice + parseInt(ticketPrice);
+
+// const grandTotal = document.getElementById("grand-total").innerText;
+// const convertGrandTotal = parseInt (grandTotal);
+// const sum2 = convertGrandTotal + parseInt(ticketPrice) ;
+
         setInnerText("seat-count", seat);
         setInnerText("seat-left", availableSeats);
         
         
+        totalCost("total-price",parseInt(ticketPrice));
+        grandTotal("grand-total",parseInt(ticketPrice));
      })
  }
+
+
+function totalCost(id,value){
+        const totalPrice = document.getElementById(id).innerText;
+        const convertTotalPrice = parseInt(totalPrice);
+        const sum = convertTotalPrice + parseInt(value);
+        setInnerText("total-price", sum);
+}
+
+function grandTotal(id,value){
+        const grandTotal = document.getElementById(id).innerText;
+        const convertGrandTotal = parseInt (grandTotal);
+        const sum = convertGrandTotal + parseInt(value) ;
+        setInnerText("grand-total", sum);
+}
 
  function setInnerText(id,value){
     document.getElementById(id).innerText = value;     
